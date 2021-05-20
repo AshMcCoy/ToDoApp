828396e6eea6a6de9a084e73c7ca0e0cc5483086
AshMcCoy <77694729+AshMcCoy@users.noreply.github.com>
2021-05-20 07:12:52 -0500

    created login template

--- ToDo_App/views.py
+++ ToDo_App/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect 
 from django.views.generic.list import ListView
 from .models import Task
-#from .models import ToDoList
 
 # def index(request):
 #     return render(request, 'index.html')
