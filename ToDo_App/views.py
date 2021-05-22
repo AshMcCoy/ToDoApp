7d0afef15a6dfded4f91ffb763de32ca3e25d48a
Tracy Hsu <xtracy@gmail.com>
2021-05-22 01:16:28 -0700

    updated notes in readme.txt

--- ToDo_App/views.py
+++ ToDo_App/views.py
@@ -8,7 +8,12 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from .models import Task, Grocery
+from .models import Task, Grocery, Bill
+from django.contrib import messages
+import bcrypt
+
+from apiclient.discovery import build 
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 class CustomLoginView(LoginView):
     template_name= 'ToDo_App/login.html'
@@ -75,7 +80,7 @@ class TaskUpdate(LoginRequiredMixin, UpdateView):
 class TaskDelete(LoginRequiredMixin, DeleteView):
     model= Task
     context_object_name= 'task'
-    success_url= reverse_lazy('tasks')
+    success_url= reverse_lazy('tasks')    
 
 class GroceryList(LoginRequiredMixin, ListView):
     model = Grocery
@@ -88,7 +93,7 @@ class GroceryList(LoginRequiredMixin, ListView):
         
         search_input = self.request.GET.get('search-area') or ''
         if search_input: 
-            context['groceries'] = context['groceries'].filter(title__startswith=search_input)
+            context['groceries'] = context['groceries'].filter(item__startswith=search_input)
 
         context['search_input'] = search_input
 
@@ -112,3 +117,75 @@ class GroceryDelete(LoginRequiredMixin, DeleteView):
     model= Grocery
     context_object_name= 'groceries'
     success_url= reverse_lazy('grocery-list')
+
+def grocery_complete(request, id):
+    mark_complete = Grocery.objects.get(id=id)
+    mark_complete.complete=True;
+    mark_complete.save()
+    return redirect('/grocery-list')
+
+def grocery_incomplete(request, id):
+    mark_complete = Grocery.objects.get(id=id)
+    mark_complete.complete=False;
+    mark_complete.save()
+    return redirect('/grocery-list')
+
+def task_complete(request, id):
+    mark_complete = Task.objects.get(id=id)
+    mark_complete.complete=True;
+    mark_complete.save()
+    return redirect('/')
+
+def task_incomplete(request, id):
+    mark_complete = Task.objects.get(id=id)
+    mark_complete.complete=False;
+    mark_complete.save()
+    return redirect('/')
+
+class BillList(LoginRequiredMixin, ListView):
+    model = Bill
+    context_object_name= 'bills'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['bills']= context['bills'].filter(user=self.request.user)
+        context['count']= context['bills'].filter(paid=False).count()
+        
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input: 
+            context['bills'] = context['bills'].filter(item__startswith=search_input)
+
+        context['search_input'] = search_input
+
+        return context
+
+class BillCreate(LoginRequiredMixin, CreateView):
+    model= Bill
+    fields = ['item', 'category', 'due_date', 'paid']
+    success_url= reverse_lazy('bill-list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(BillCreate, self).form_valid(form)
+
+class BillUpdate(LoginRequiredMixin, UpdateView):
+    model= Bill
+    fields= ['item', 'category', 'due_date', 'paid']
+    success_url= reverse_lazy('bill-list')
+
+class BillDelete(LoginRequiredMixin, DeleteView):
+    model= Bill
+    context_object_name= 'bills'
+    success_url= reverse_lazy('bill-list')
+
+def bill_paid(request, id):
+    mark_paid = Bill.objects.get(id=id)
+    mark_paid.paid=True;
+    mark_paid.save()
+    return redirect('/bill-list')
+
+def bill_notpaid(request, id):
+    mark_notpaid = Bill.objects.get(id=id)
+    mark_notpaid.paid=False;
+    mark_notpaid.save()
+    return redirect('/bill-list')
